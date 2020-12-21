0823c7cd49e331e681cc4ef338eb3acdd25154b4
PutraBudiansyah <pbudiansyah72@gmail.com>
2020-12-21 14:51:14 +0700

    initial commit
    initial commit

---
+++ Assets/Controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+    UniWebView webView;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Create a game object to hold UniWebView and add component.
+        var webViewGameObject = new GameObject("UniWebView");
+        webView = webViewGameObject.AddComponent<UniWebView>();
+
+        // More to add...
+        webView.Frame = new Rect(0, 0, Screen.width, Screen.height);
+        webView.Load("https://reacttodobudiansyah.000webhostapp.com/");
+        webView.Show();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
