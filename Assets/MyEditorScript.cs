5166a8ba1275ad982bf8277bcf007c41723440ec
PutraBudiansyah <60534181+PutraBudiansyah@users.noreply.github.com>
2021-01-07 20:36:36 +0700

    Create MyEditorScript.cs

---
+++ Assets/MyEditorScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+
+public class MyEditorScript : MonoBehaviour
+{
+    
+    /*static void PerformBuild ()
+     {
+        string[] defaultScenes = { "Assets/Scenes/Demo.unity" };
+         BuildPipeline.BuildPlayer(defaultScenes,"./builds/",
+         BuildTarget.Android);
+     }*/
+
+    [MenuItem("Assets")]
+    public static void PerformBuild()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scenes/Demo.unity"};
+        buildPlayerOptions.locationPathName = "Build";
+        buildPlayerOptions.target = BuildTarget.Android;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+
+     
+}
